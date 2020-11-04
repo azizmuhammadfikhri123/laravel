1ab7f6c2214fdff84415955c5d02f73382e3db16
azizmuhammadfikhri123 <fikhrimuhammadaziz@gmail.com>
2020-11-04 13:47:22 +0700

    menghapus dropdown icon bendera

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -98,21 +98,7 @@
                     <div class="language-select dropdown" id="language-select">
                         <a class="dropdown-toggle" href="#" data-toggle="dropdown"  id="language" aria-haspopup="true" aria-expanded="true">
                             <i class="flag-icon flag-icon-id"></i>
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="language" >
-                            <div class="dropdown-item">
-                                <span class="flag-icon flag-icon-id"></span>
-                            </div>
-                            <div class="dropdown-item">
-                                <i class="flag-icon flag-icon-es"></i>
-                            </div>
-                            <div class="dropdown-item">
-                                <i class="flag-icon flag-icon-us"></i>
-                            </div>
-                            <div class="dropdown-item">
-                                <i class="flag-icon flag-icon-jp"></i>
-                            </div>
-                        </div>
+                        </a>      
                     </div>
 
                 </div>
